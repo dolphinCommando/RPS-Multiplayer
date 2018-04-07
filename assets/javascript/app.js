3b426bb8908f6fe26bb677cc3e5938f009718b36
Matthew Buchanan <matt@matthews-air.home>
2018-04-07 16:25:37 -0400

    sources

---
+++ assets/javascript/app.js
@@ -0,0 +1,10 @@
+// Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyClHmL8O28isW_xz1f5yaUgmURcdKhW-7I",
+    authDomain: "multi-rps-5c6a8.firebaseapp.com",
+    databaseURL: "https://multi-rps-5c6a8.firebaseio.com",
+    projectId: "multi-rps-5c6a8",
+    storageBucket: "",
+    messagingSenderId: "362312322057"
+  };
+  firebase.initializeApp(config);
