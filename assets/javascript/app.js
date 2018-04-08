d9ff1bb3106f83f09ef05952ab12ebe0731ad42c
Matthew Buchanan <matt@matthews-air.home>
2018-04-08 10:19:24 -0400

    chat box

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,17 +9,18 @@
   };
   firebase.initializeApp(config);
 
-  database = firebase.database();
+  
 
-  var connectedRef = firebase.database().ref('.info/connected');
+  const database = firebase.database();
+
+  const connectedRef = firebase.database().ref('.info/connected');
   connectedRef.on('value', function(snap) {
     if (snap.val() === true) {
       console.log('Connected');
       console.log(snap.val());
     } else {
       console.log('Disconnected');
-      console.log(snap.val());
-      
+      console.log(snap.val());    
     }
   });
 
@@ -27,17 +28,43 @@
     event.preventDefault();
     var playerName = $('#player-get').val();
     var index = ''
-    var newRef = database.ref('players').push();
+    var newRef = database.ref('players').child(`${playerName}`);
     newRef.set({
-           'name': playerName,
-           'choice': '',
            'losses': 0,
            'wins': 0 
 
       });
-      $('#player-name').html(`<p>Welcome ${playerName}</p>`);
+      $('#player-container').html(`<p>Welcome <span id="player-name">${playerName}</span></p>`);
+  });
+  
+  $('#chat-submit').click(function(event) {
+    event.preventDefault();
+    var name = $('#player-name').text();
+    var chatIn = $('#chat-input').val();
+    var newRef = database.ref('chat').push();
+    newRef.set({
+      name: `${name}`,
+      text: `${chatIn}`
+    });
+    $('chat-input').val('');
   });
 
- 
-    
+  database.ref('chat').on('child_added', function(snapshot) {
+    $('#chat-output').append(`${snapshot.val().name}: ${snapshot.val().text}<br>`);
 
+  });
+
+
+
+  database.ref('players').on('child_added', function(snapshot) {
+    console.log('Child added ' + JSON.stringify(snapshot.val()));
+  });
+
+  database.ref('players').on('value', function(snapshot) {
+    console.log('Players value ' + JSON.stringify(snapshot.val()));
+    console.log(snapshot.val());
+  });
+
+  
+
+  
