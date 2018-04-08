28dd54cad8e0cab2a7065df8eb3d9c48a2334551
Matthew Buchanan <matt@matthews-air.home>
2018-04-07 21:34:23 -0400

    app.js detects connection state and uploads player name

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,36 @@
     messagingSenderId: "362312322057"
   };
   firebase.initializeApp(config);
+
+  database = firebase.database();
+
+  var connectedRef = firebase.database().ref('.info/connected');
+  connectedRef.on('value', function(snap) {
+    if (snap.val() === true) {
+      console.log('Connected');
+      console.log(snap.val());
+    } else {
+      console.log('Disconnected');
+      console.log(snap.val());
+      
+    }
+  });
+
+  $('#player-submit').click(function(event) {
+    event.preventDefault();
+    var playerName = $('#player-get').val();
+    var index = ''
+    var newRef = database.ref('players').push();
+    newRef.set({
+           'name': playerName,
+           'choice': '',
+           'losses': 0,
+           'wins': 0 
+
+      });
+      $('#player-name').html(`<p>Welcome ${playerName}</p>`);
+  });
+
+ 
+    
+
