c522d2d6da67e0116b3327d4957b86cccdf619a3
Matthew Buchanan <matt@matthews-air.home>
2018-04-08 22:08:08 -0400

    board renders

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,9 +9,12 @@
   };
   firebase.initializeApp(config);
 
-  
+  var index=0;
+  var playerIndex = index;
 
   const database = firebase.database();
+
+  database.ref().child('index').set({'value': index});
 
   const connectedRef = firebase.database().ref('.info/connected');
   connectedRef.on('value', function(snap) {
@@ -27,11 +30,13 @@
   $('#player-submit').click(function(event) {
     event.preventDefault();
     var playerName = $('#player-get').val();
-    var index = ''
-    var newRef = database.ref('players').child(`${playerName}`);
+    var newRef = database.ref().child('players').push();
     newRef.set({
+           'name': playerName,
            'losses': 0,
-           'wins': 0 
+           'wins': 0,
+           'choice': '',
+           'playerindex': playerIndex
 
       });
       $('#player-container').html(`<p>Welcome <span id="player-name">${playerName}</span></p>`);
@@ -58,12 +63,45 @@
 
   database.ref('players').on('child_added', function(snapshot) {
     console.log('Child added ' + JSON.stringify(snapshot.val()));
+    database.ref('index').set({
+      'value': (index + 1)%2
+    });
+    renderBoard(snapshot.child('playerindex').val(), snapshot.child('name').val(), snapshot.child('wins').val(), snapshot.child('losses').val(), snapshot.child('choice').val());
+    
   });
 
   database.ref('players').on('value', function(snapshot) {
     console.log('Players value ' + JSON.stringify(snapshot.val()));
-    console.log(snapshot.val());
+    console.log('Snapshot val' + JSON.stringify(snapshot.val()));
+    //console.log('Snapshot index key '+ snapshot.child('index').val());
+    
   });
+
+  database.ref('index').on('value', function(snapshot) {
+    index = snapshot.val().value;  
+    playerIndex = index;
+  })
+
+
+  function renderBoard(idx, name, wins, losses, choice) {
+    var playerBoard;
+    if (idx === 0) {
+      playerBoard = $('#game-player1');
+    } 
+    else if (idx === 1) {
+      playerBoard = $('#game-player2');
+    } else {console.log('Error: index out of bounds ' + idx);}
+
+    playerBoard.html(`
+      <p data-name="${name}">${name}</p>
+      <p class="choice">Rock</p>
+      <p class="choice">Paper</p>
+      <p class="choice">Scissors</p>
+      <p>Wins: ${wins}  Losses: ${losses}</p>
+    `)
+  }
+
+  
 
   
 
